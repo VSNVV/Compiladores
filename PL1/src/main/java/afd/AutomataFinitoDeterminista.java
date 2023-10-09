d753b7c5660036c7f26c32bdc684f5354b00f337
viti <victor.sanavia@edu.uah.es>
2023-10-09 12:11:08 +0200

    Creacion de AutomataFinitoDeterminista
    He hecho posibles "bocetos" de cómo pueden ser las funciones del autómata, luego les daré mi toque personal

---
+++ PL1/src/main/java/afd/AutomataFinitoDeterminista.java
@@ -0,0 +1,116 @@
+package afd;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class AutomataFinitoDeterminista {
+    //Atributos de la clase AutomataFinitoDeterminista
+    private final List<Character> alfabeto; //Lista de elementos
+    private List<Integer> estados; //Los estados serán representados mediante enteros
+    private List<Integer> estadosFinales; //Almacena los estados finales
+    private Integer estadoInicial;
+    private HashMap<Integer, HashMap<Character, Integer>> matriz; //Matriz de estados
+
+    //Métodos de la clase AutomataFinitoDeterminista
+
+    //Constructor
+    public AutomataFinitoDeterminista(){
+        this.alfabeto = new ArrayList<>();
+        this.estados = new ArrayList<>();
+        this.estadosFinales = new ArrayList<>();
+        this.estadoInicial = 0;
+        this.matriz = new HashMap<>();
+    }
+
+    //Método para cargar el alfabeto
+    public void cargaAlfabeto(){
+        //Posible boceto --> meterle como parámetro una variable de tipo String y de ahi bailar como vinisiu o rei, sacando
+        //el for simplemente midiendo cuanto mide nuestro string
+
+    }
+
+    //Método para cargar los estados
+    public void cargaEstados(){
+        //Posible boceto --> Igual que la funcion de arriba, pero en este caso los estados pueden ser numeros enteros
+        //consecutivos y en este caso no haría falta meter ningún parámetro, simplemente podemos ahcer un for que
+        //recorra ciertos numeros y que nos la sude completamente
+    }
+
+    //Método para establecer el estado inicial
+    public void estableceEstadoInicial(int estadoInicial){
+        //Simplemente establecemos el estado inicial usando el setter que creamos mas abajo:
+        setEstadoInicial(estadoInicial);
+    }
+
+    //Método para establecer los estados finales
+    public void estableceEstadosFinales(){
+        //Posible boceto --> Aqui podems tener muchos estados finales, asi que de alguna manera tenemos que pasarle
+        //una lista con todos los estados finales que podemos tener, entonces harmeos un for con todos los estados
+        //finales que tengamos dentro de una lista
+    }
+
+    //Método para inicializar la matriz
+    public void inicializaMatriz(){
+        int estado;
+        //Tendremos que recorrer el arraylist de estados y meterlos todos
+        for(int index = 0; index < getEstados().size(); index++){
+            estado = getEstados().get(index);
+            getMatriz().put(estado, new HashMap<Character, Integer>()); //Añadimos con put
+        }
+        //Una vez que tengamos la matriz principal hecha, tenemos que ir a por la anidada
+
+    }
+
+    //Método para ver el siguientr paso
+    public void siguientePaso(){
+        //Sería leer de la matriz dados un estado y un carcater, con estos dos datos tendriamos que ir a la matriz
+        //y leer cual es el siguiente estado al que nos estaremos metiendo
+    }
+
+
+
+
+
+
+
+
+
+
+    //Métodos get y set
+    public List<Character> getAlfabeto() {
+        return alfabeto;
+    }
+
+    public List<Integer> getEstados() {
+        return estados;
+    }
+
+    public void setEstados(List<Integer> estados) {
+        this.estados = estados;
+    }
+
+    public List<Integer> getEstadosFinales() {
+        return estadosFinales;
+    }
+
+    public void setEstadosFinales(List<Integer> estadosFinales) {
+        this.estadosFinales = estadosFinales;
+    }
+
+    public Integer getEstadoInicial() {
+        return estadoInicial;
+    }
+
+    public void setEstadoInicial(Integer estadoInicial) {
+        this.estadoInicial = estadoInicial;
+    }
+
+    public HashMap<Integer, HashMap<Character, Integer>> getMatriz() {
+        return matriz;
+    }
+
+    public void setMatriz(HashMap<Integer, HashMap<Character, Integer>> matriz) {
+        this.matriz = matriz;
+    }
+}
