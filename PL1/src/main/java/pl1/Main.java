9be5110db15afe02b32da34eed58945b40e93d3e
viti <victor.sanavia@edu.uah.es>
2023-10-12 22:41:33 +0200

    Boceto de Main pendiente de remodelar

--- PL1/src/main/java/pl1/Main.java
+++ PL1/src/main/java/pl1/Main.java
@@ -11,7 +11,7 @@ public class Main {
         //El alfabeto del AFD1 tiene a, b y c
         String alfabetoAfd1 = "abc";
         //El AFD1 tiene 5 estados
-        List<Integer> listaEstadosAfd1 = new ArrayList<>(Arrays.asList(0, 1, 2, 3, 4)); //El autómata tiene 5 estados
+        List<Integer> listaEstadosAfd1 = new ArrayList<>(Arrays.asList(0, 1, 2, 3, 4, 5, 6)); //El autómata tiene 5 estados
         //El AFD1 solo tiene un estado final que es el estado 4
         List<Integer> listaEstadosFinalesAfd1 = new ArrayList<>(Collections.singletonList(5));
         //Ahora crearemos las características del segundo AFD
@@ -19,9 +19,9 @@ public class Main {
         //El alfabeto del AFD2 tiene a, b, c, d
         String alfabetoAfd2 = "abcd";
         //El AFD2 tiene 6 estados
-        List<Integer> listaEstadosAfd2 = new ArrayList<>(Arrays.asList(0, 1, 2, 3, 4, 5)); //El autómata tiene 5 estados
+        List<Integer> listaEstadosAfd2 = new ArrayList<>(Arrays.asList(0, 1, 2, 3, 4, 5, 6)); //El autómata tiene 5 estados
         //El AFD2 tiene 2 estados finales, que son el 4 y el 5
-        List<Integer> listaEstadosFinalesAfd2 = new ArrayList<>(Arrays.asList(4, 5));
+        List<Integer> listaEstadosFinalesAfd2 = new ArrayList<>(Arrays.asList(5, 6));
 
         //Una vez que tenemos las características creadas de cada autómata, los creamos
         //Creamos el AFD1
@@ -31,25 +31,31 @@ public class Main {
 
         //Ahora rellenaremos la matriz de estados de ambos autómatas según la ER que maneja cada uno (visible en la memoria)
 
-        //Matriz de AFD1
-        afd1.cargarMatriz(0, 'a', 2); //Estado 0 + a --> Estado 2
-        afd1.cargarMatriz(0, 'c', 0); //Estado 0 + c --> Estado 0
+        //Matriz del AFD1
+        afd1.cargarMatriz(0, 'c', 2); //Estado 0 + c --> Estado 2
+        afd1.cargarMatriz(1, 'a', 1); //Estado 1 + a --> Estado 1
         afd1.cargarMatriz(1, 'b', 3); //Estado 1 + b --> Estado 3
-        afd1.cargarMatriz(2, 'a', 2); //Estado 2 + a --> Estado 2
-        afd1.cargarMatriz(2, 'b', 3); //Estado 2 + b --> Estado 3
-        afd1.cargarMatriz(2, 'c', 0); //Estado 2 + c --> Estado 0
-        afd1.cargarMatriz(3, 'a', 1); //Estado 3 + a --> Estado 1
-        afd1.cargarMatriz(3, 'b', 4); //Estado 3 + b --> Estado 4
+        afd1.cargarMatriz(1, 'c', 2); //Estado 1 + c --> Estado 2
+        afd1.cargarMatriz(2, 'a', 1); //Estado 2 + a --> Estado 1
+        afd1.cargarMatriz(2, 'c', 2); //Estado 2 + c --> Estado 2
+        afd1.cargarMatriz(3, 'a', 4); //Estado 3 + 3 --> Estado 4
+        afd1.cargarMatriz(3, 'b', 5); //Estado 3 + b --> Estado 5
+        afd1.cargarMatriz(4, 'b', 6); //Estado 4 + b --> Estado 6
+        afd1.cargarMatriz(6, 'a', 4); //Estado 6 + a --> Estado 4
+        afd1.cargarMatriz(6, 'b', 5); //Estado 6 + b --> Estado 5
 
-        //Matriz de AFD2
+        //Matriz del AFD2
         afd2.cargarMatriz(0, 'c', 1); //Estado 0 + c --> Estado 1
-        afd2.cargarMatriz(1, 'c', 1); //Estado 1 + c --> Estado 1
-        afd2.cargarMatriz(1, 'd', 3); //Estado 1 + d --> Estado 3
-        afd2.cargarMatriz(2, 'd', 4); //Estado 2 + d --> Estado 4
-        afd2.cargarMatriz(3, 'a', 5); //Estado 3 + a --> Estado 5
-        afd2.cargarMatriz(3, 'b', 2); //Estado 3 + b --> Estado 2
+        afd2.cargarMatriz(1, 'c', 3); //Estado 1 + c --> Estado 3
+        afd2.cargarMatriz(1, 'd', 2); //Estado 1 + d --> Estado 2
+        afd2.cargarMatriz(2, 'a', 5); //Estado 2 + a --> Estado 5
+        afd2.cargarMatriz(2, 'b', 4); //Estado 2 + b --> Estado 4
+        afd2.cargarMatriz(3, 'c', 3); //Estado 3 + c --> Estado 3
+        afd2.cargarMatriz(3, 'd', 2); //Estado 3 + d --> Estado 2
+        afd2.cargarMatriz(4, 'd', 6); //Estado 4 + d --> Estado 6
         afd2.cargarMatriz(5, 'a', 5); //Estado 5 + a --> Estado 5
-        afd2.cargarMatriz(5, 'b', 2); //Estado 5 + b --> Estado 2
+        afd2.cargarMatriz(5, 'b', 4); //Estado 5 + b --> Estado 4
+
 
         //Una vez creadas las matrices de estados de ambos autómatas, los asignamos a sus respectivas máquinas de estados
         MaquinaEstados me1 = new MaquinaEstados(afd1); //Creamos la máquina de estados del autómata AFD1
@@ -57,8 +63,10 @@ public class Main {
 
         boolean corriendo = true;
         Scanner entrada = new Scanner(System.in);
-        MaquinaEstados me;
+        me1.generaCadenasValidas();
+        /*
         while(corriendo){
+            MaquinaEstados me;
             System.out.print("\t\t--------------=[PL1 Compiladores]=--------------\n" +
                     "\t¿Que automata desea elegir? (1 o 2)");
             String automata = entrada.nextLine();
@@ -70,11 +78,40 @@ public class Main {
                 me = me2;
             }
             else{
-                System.out.println("Eleccion no valida, debes introducir 1 o 2<");
+                System.out.println("Eleccion no valida, debes introducir 1 o 2");
             }
+            System.out.print("\t\t--------------=[PL1 Compiladores]=--------------\n" +
+                    "\t1. Validar cadena\n" +
+                    "\t2. Generar cadenas validas\n" +
+                    "\t3. Salir" +
+                    "\n" +
+                    "Introduce una opcion: ");
+            String eleccion = entrada.nextLine();
+            if(eleccion.equalsIgnoreCase("1")){
+                //Se verifica que se quiere validar una cadena
+                System.out.print("\n\nIntroduce la cadena a validar: ");
+                String cadena = entrada.nextLine();
+                if(automata.equalsIgnoreCase("1")){
+                    if(me1.compruebaCadena(cadena)){
+                        System.out.println("La cadena es valida");
+                    }
+                    else{
+                        System.out.println("La cadena no es valida");
+                    }
+                }
+                else{
+                    if(me2.compruebaCadena(cadena)){
+                        System.out.println("La cadena es valida");
+                    }
+                    else{
+                        System.out.println("La cadena no es valida");
+                    }
+                }
+            }
+            else if(eleccion.equalsIgnoreCase("2")){
+                //Se verifica que se quiere generan cadenas válidas
 
-
-        }
-
+            }
+        }*/
     }
 }
