9a87cb43fa8109b49caa3bb8c663a4eea8f6fee5
viti <victor.sanavia@edu.uah.es>
2023-12-12 15:48:39 +0100

    Eliminado el PL3Compiladores
    Estamos usando el proyecto llamado ProyectoVisitors, este no valía para nada

--- PL3/Visitors/PL3Compiladores/src/main/java/MiVisitor.java
+++
@@ -1,8 +0,0 @@
-//Visitor específico para calcular las cosas
-public class MiVisitor extends LinguineParserBaseVisitor<String>{
-
-    public String visitPrograma(LinguineParser.ProgramaContext ctx) {
-        return visitChildren(ctx);
-    }
-
-}
